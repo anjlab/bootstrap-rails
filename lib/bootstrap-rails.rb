bc3d90456327c61e6f0ae80b80eeb19c2798af15
davidsulc <david@davidsulc.com>
2011-11-30 18:58:16 -0500

    fix gradients in IE
    IE doesn't process gradient colors like '#333' in the DXImageTransform.Microsoft.gradient function (it considers it as '#000333' instead of '#333333')
    
    add a Sass method to pad color arguments and pass strings to the DXImageTransform.Microsoft.gradient function. This allows for the principle of least surprise: if a user calls a mixin with a 3-digit color argument, transparently convert it to a 6-digit argument behind the scenes

--- lib/bootstrap-rails.rb
+++ lib/bootstrap-rails.rb
@@ -2,6 +2,7 @@ require "bootstrap-rails/version"
 
 module Bootstrap
   module Rails
+    require "bootstrap-rails/ie_hex_str"
     if ::Rails.version < "3.1"
       require "bootstrap-rails/railtie"
     else
---
+++ lib/bootstrap-rails/ie_hex_str.rb
@@ -0,0 +1,21 @@
+require "sass"
+
+module Sass::Script::Functions
+  # returns an IE hex string for a color with an alpha channel
+  # suitable for passing to IE filters.
+  # see http://msdn.microsoft.com/en-us/library/ms532930%28v=vs.85%29.aspx
+  def ie_hex_str(color)
+    assert_type color, :Color
+    alpha = (color.alpha * 255).round
+    alpha = alpha.to_s(16).rjust(2, '0')
+    color_string = color.to_s
+    color_values = color_string.tr('#','').split('')
+    
+    r, g, b = *color_values
+    if color_values.size == 3
+      color_string = "#{r}#{r}#{g}#{g}#{b}#{b}"
+    end
+    Sass::Script::String.new("##{alpha}#{color_string}")
+  end
+  declare :ie_hex_str, :args => [:color]
+end
