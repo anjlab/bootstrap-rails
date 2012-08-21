34e18dace6f4246e878cd3a99063c5dce941401a
yury <yury.korolev@gmail.com>
2012-08-21 16:17:54 +0400

    Update vendorerd resources

--- vendor/assets/javascripts/bootstrap.js
+++ vendor/assets/javascripts/bootstrap.js
@@ -1,12 +1,13 @@
 //= require bootstrap-transition.js
 //= require bootstrap-dropdown.js
+//= require bootstrap-button.js
 //= require bootstrap-carousel.js
 //= require bootstrap-collapse.js
-//= require bootstrap-button.js
-//= require bootstrap-modal.js
 //= require bootstrap-alert.js
+//= require bootstrap-affix.js
+//= require bootstrap-typeahead.js
 //= require bootstrap-scrollspy.js
 //= require bootstrap-tab.js
-//= require bootstrap-typeahead.js
+//= require bootstrap-modal.js
 //= require bootstrap-tooltip.js
 //= require bootstrap-popover.js
