c9d4939548d6f833406c0ca52c43d5f2eee242ec
yury <yury.korolev@gmail.com>
2012-08-23 14:05:46 +0400

    Try to fix loading issue

--- lib/bootstrap-rails/engine.rb
+++ lib/bootstrap-rails/engine.rb
@@ -1,7 +1,7 @@
 module Bootstrap
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'anjlab-bootstrap-rails.setup' do |app|
+      initializer 'anjlab-bootstrap-rails.setup' => 'setup_sass', group: :all do |app|
         if config.respond_to? :sass
           config.sass.load_paths << File.join(config.root, 'vendor', 'frameworks')
         end
--- lib/bootstrap-rails/version.rb
+++ lib/bootstrap-rails/version.rb
@@ -1,5 +1,5 @@
 module Bootstrap
   module Rails
-    VERSION = "2.1.0.1"
+    VERSION = "2.1.0.2"
   end
 end
