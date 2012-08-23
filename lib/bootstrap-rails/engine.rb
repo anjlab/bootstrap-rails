9182e777aafb258f3741ff82f0672d33245de48d
yury <yury.korolev@gmail.com>
2012-08-23 12:11:40 +0400

    Workaround production problem. Closes #26

--- lib/bootstrap-rails/engine.rb
+++ lib/bootstrap-rails/engine.rb
@@ -2,7 +2,9 @@ module Bootstrap
   module Rails
     class Engine < ::Rails::Engine
       initializer 'anjlab-bootstrap-rails.setup' do |app|
-        config.sass.load_paths << File.join(config.root, 'vendor', 'frameworks')
+        if config.respond_to? :sass
+          config.sass.load_paths << File.join(config.root, 'vendor', 'frameworks')
+        end
       end
     end
   end
