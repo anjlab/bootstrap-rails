490521da0664b76c917f0715748864e2145201db
yury <yury.korolev@gmail.com>
2013-01-04 01:05:33 +0400

    Get ready for rails 4

--- lib/bootstrap-rails/engine.rb
+++ lib/bootstrap-rails/engine.rb
@@ -2,8 +2,14 @@ module Bootstrap
   module Rails
     class Engine < ::Rails::Engine
       initializer 'anjlab-bootstrap-rails-setup', :group => :all do |app|
+        frameworks_path = File.join(config.root, 'vendor', 'frameworks')
+        # rails 3.2.x
         if config.respond_to? :sass
-          config.sass.load_paths << File.join(config.root, 'vendor', 'frameworks')
+          config.sass.load_paths << frameworks_path
+        end
+        # rails 4.0.x
+        if config.respond_to? :assets
+          config.assets.paths << frameworks_path
         end
       end
     end
